fc42136b2a1e1054289440f2587842f4a3546af7
Louis-Saglio <louis.saglio@sfr.fr>
2018-01-05 14:01:23 +0100

    You can add comment on an article

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,8 +2,13 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\comment;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class MainController extends Controller
@@ -14,17 +19,42 @@ class MainController extends Controller
     public function indexAction()
     {
         $repo = $this->getDoctrine()->getManager()->getRepository("AppBundle:article");
-        return $this->render("@App/index.html.twig", ['articles'=>$repo->findAll()]);
+
+        return $this->render("@App/index.html.twig", [
+            'articles'=> $repo->findAll()
+        ]);
     }
 
     /**
      * @Route("/article/{id}")
+     * @param Request $request
+     * @param $id
+     * @return Response
      */
-    public function ArticleView($id)
+    public function ArticleView(Request $request, $id)
     {
-        $repo = $this->getDoctrine()->getManager()->getRepository("AppBundle:article");
-        return$this->render("@App/articleView.html.twig", ['article'=>$repo->find($id)]);
+        $em = $this->getDoctrine()->getManager();
+
+        $comment = new comment();
+
+        $form = $this->get("form.factory")->createBuilder(FormType::class, $comment)
+            ->add('body', TextareaType::class)
+            ->add('Poster', SubmitType::class)
+            ->getForm()
+        ;
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+            $comment->setArticleId($id);
+            if ($form->isValid()) {
+                $em->persist($comment);
+                $em->flush();
+            }
+        }
+
+        return $this->render("@App/articleView.html.twig", [
+            'article' => $em->getRepository("AppBundle:article")->find($id),
+            'form' => $form->createView()
+        ]);
     }
-
-
 }
---
+++ src/AppBundle/Entity/comment.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * comment
+ *
+ * @ORM\Table(name="comment")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\commentRepository")
+ */
+class comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text")
+     */
+    private $body;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="article_id", type="integer")
+     */
+    private $articleId;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set body
+     *
+     * @param string $body
+     *
+     * @return comment
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get body
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set articleId
+     *
+     * @param integer $articleId
+     *
+     * @return comment
+     */
+    public function setArticleId($articleId)
+    {
+        $this->articleId = $articleId;
+
+        return $this;
+    }
+
+    /**
+     * Get articleId
+     *
+     * @return int
+     */
+    public function getArticleId()
+    {
+        return $this->articleId;
+    }
+}
+
